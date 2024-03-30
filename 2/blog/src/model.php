3eac3b2997b13a3cb17bf14f18de28085614aa61
Guillaume Debas <guillaumedebas@hotmail.fr>
2024-03-30 16:14:17 +0100

    add_comment added

--- 2/blog/src/model.php
+++ 2/blog/src/model.php
@@ -47,33 +47,8 @@ function getPost($identifier) {
         'title' => $row['title'],
         'french_creation_date' => $row['french_creation_date'],
         'content' => $row['content'],
+        'identifier' => $row['id'],
     ];
  
     return $post;
-}
-
-function getComments($identifier)
-{
-    try {
-        $database = new PDO('mysql:host=localhost;dbname=oldblog;charset=utf8', 'root', '');
-    } catch(Exception $e) {
-        die('Erreur : '.$e->getMessage());
-    }
- 
-    $statement = $database->prepare(
-        "SELECT id, author, comment, DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM comments WHERE post_id = ? ORDER BY comment_date DESC"
-    );
-    $statement->execute([$identifier]);
- 
-    $comments = [];
-    while (($row = $statement->fetch())) {
-        $comment = [
-            'author' => $row['author'],
-            'french_creation_date' => $row['french_creation_date'],
-            'comment' => $row['comment'],
-        ];
-        $comments[] = $comment;
-    }
-
-    return $comments;
 }
--- 2/blog/templates/post.php
+++ 2/blog/templates/post.php
@@ -12,40 +12,54 @@
     <h1>Le super blog de l'AVBN !</h1>
     <p>Derniers billets du blog :</p>
 
-  
-        <div class="news">
-            <h3>
-                <?= htmlspecialchars($post['title']); ?>
-                <em>le <?= $post['french_creation_date']; ?></em>
-            </h3>
-            <p>
-                <?= nl2br(htmlspecialchars($post['content']));
-                ?>
-                <br />
-                <em>
-            Commentaires
-                </em>
 
-                <?php
-                foreach ($comments as $comment) {
-                ?>
-           
-                <h4>
-                    <?= htmlspecialchars($comment['author']); ?>
-                    <em>le <?= $comment['french_creation_date']; ?></em>
-                </h4>
-                <p>
-                    <?= nl2br(htmlspecialchars($comment['comment']));
-                    ?>
-                    <br />
+    <div class="news">
+        <h3>
+            <?= htmlspecialchars($post['title']); ?>
+            <em>le <?= $post['french_creation_date']; ?></em>
+        </h3>
+        <p>
+            <?= nl2br(htmlspecialchars($post['content']));
+            ?>
+            <br />
+            <em>
+                Commentaires
+            </em>
 
-                </p>
-         
-        <?php
-                } // The end of the posts loop.
-        ?>
+            <?php
+            foreach ($comments as $comment) {
+            ?>
+
+        <h4>
+            <?= htmlspecialchars($comment['author']); ?>
+            <em>le <?= $comment['french_creation_date']; ?></em>
+        </h4>
+        <p>
+            <?= nl2br(htmlspecialchars($comment['comment']));
+            ?>
+            <br />
+
         </p>
+
+    <?php
+            } // The end of the posts loop.
+    ?>
+    <h2>Commentaires</h2>
+    <form action="index.php?action=addComment&id=<?= $post['identifier'] ?>" method="post">
+        <div>
+            <label for="author">Auteur</label><br />
+            <input type="text" id="author" name="author" />
         </div>
+        <div>
+            <label for="comment">Commentaire</label><br />
+            <textarea id="comment" name="comment"></textarea>
+        </div>
+        <div>
+            <input type="submit" />
+        </div>
+    </form>
+    </p>
+    </div>
 </body>
 
 </html>
