91e2773c8294d705355b789bd4c1ad7ea9cf0275
Guillaume Debas <guillaumedebas@hotmail.fr>
2024-03-31 23:27:09 +0200

    error system added

--- 2/blog/src/model/comment.php
+++ 2/blog/src/model/comment.php
@@ -36,11 +36,9 @@ function createComment(string $post, string $author, string $comment)
 
 function commentDbConnect()
 {
-	try {
+	
     	$database = new PDO('mysql:host=localhost;dbname=oldblog;charset=utf8', 'root', '');
 
     	return $database;
-	} catch(Exception $e) {
-    	die('Erreur : '.$e->getMessage());
-	}
+	
 }
